dc23b1cb60451fea193813c7eb30b45c41a95adb
AnhKhoa Nguyen <anhkhoa.nguyen@sioux.asia>
2014-11-17 15:45:28 +0700

    Change path of android app

---
+++ Trunk/Software/Android/MachLeo/src/com/assignment/machleo/customizedviews/SampleCustomizedView.java
@@ -0,0 +1,8 @@
+package com.assignment.machleo.customizedviews;
+
+public class SampleCustomizedView {
+	/*
+	 * If there's any customized control, define it here.
+	 * Each control is one class
+	 */
+}
---
+++ Trunk/Software/Android/MachLeo/src/com/assignment/machleo/models/SampleModel.java
@@ -0,0 +1,5 @@
+package com.assignment.machleo.models;
+
+public class SampleModel {
+	// Define all models in this packet, each model is in one class
+}
