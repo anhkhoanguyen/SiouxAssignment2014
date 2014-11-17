db6c6e6bd74ca0943ab863d709465118a598f570
AnhKhoa Nguyen <anhkhoa.nguyen@sioux.asia>
2014-11-17 15:41:54 +0700

    Add MachLeo android structure

---
+++ Trunk/Software/MachLeo/src/com/assignment/machleo/controllers/ServiceTransfer.java
@@ -0,0 +1,6 @@
+package com.assignment.machleo.controllers;
+
+public class ServiceTransfer {
+	// Define all things related to web service here
+	// Add more controller to do other things in another class
+}
---
+++ Trunk/Software/MachLeo/src/com/assignment/machleo/helpers/SampleHelper.java
@@ -0,0 +1,8 @@
+package com.assignment.machleo.helpers;
+
+public class SampleHelper {
+	/*
+	 * Define all helpers in this packet, each helper is in one class
+	 * Helpers are thing that do some general things
+	 */
+}
